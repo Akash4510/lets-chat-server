518499f7d994d92de48482ff7f7c348096d030fe
Akash <ag308669@gmail.com>
2023-03-24 17:41:05 +0530

    Created user controller

---
+++ controllers/user.js
@@ -0,0 +1,24 @@
+const User = require('../models/user');
+
+exports.updateMe = async function (req, res, next) {
+  const { user } = req;
+
+  const filteredBody = filterObject(
+    req.body,
+    'firstName',
+    'lastName',
+    'about',
+    'avatar'
+  );
+
+  const updatedUser = await User.findByIdAndUpdate(user._id, filteredBody, {
+    new: true,
+    validateModifyOnly: true,
+  });
+
+  return res.status(200).json({
+    status: 'success',
+    data: updatedUser,
+    message: 'Profile updated successfully!',
+  });
+};
