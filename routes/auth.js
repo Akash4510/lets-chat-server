892ef6c8442f1db861e57a21df36314d2c96f4ff
Akash <ag308669@gmail.com>
2023-03-25 12:25:49 +0530

    Added send otp middleware in register route

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const authController = require('../controllers/auth');
 
 router.post('/login', authController.login);
-router.post('/register', authController.register);
+router.post('/register', authController.register, authController.sendOtp);
 router.post('/send-otp', authController.sendOtp);
 router.post('/verify-otp', authController.verifyOtp);
 router.post('/forgot-password', authController.forgotPassword);
