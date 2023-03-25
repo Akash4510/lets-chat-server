098e260e2e17724a30e21e4e74b0a8d1a5ac7caf
Akash <ag308669@gmail.com>
2023-03-25 12:13:20 +0530

    Commented out sg mail

--- services/mailer.js
+++ services/mailer.js
@@ -1,35 +1,35 @@
-const sgMail = require('@sendgrid/mail');
-const dotenv = require('dotenv');
+// const sgMail = require('@sendgrid/mail');
+// const dotenv = require('dotenv');
 
-dotenv.config({ path: './config.env' });
+// dotenv.config({ path: './config.env' });
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const sendSGMail = async ({
-  recipient,
-  sender,
-  subject,
-  html,
-  text,
-  attachments,
-}) => {
-  try {
-    const from = sender || process.env.SENDGRID_EMAIL;
+// const sendSGMail = async ({
+//   recipient,
+//   sender,
+//   subject,
+//   html,
+//   text,
+//   attachments,
+// }) => {
+//   try {
+//     const from = sender || process.env.SENDGRID_EMAIL;
 
-    const msg = {
-      to: recipient,
-      from,
-      subject,
-      html,
-      text,
-      attachments,
-    };
+//     const msg = {
+//       to: recipient,
+//       from,
+//       subject,
+//       html,
+//       text,
+//       attachments,
+//     };
 
-    return await sgMail.send(msg);
-  } catch (error) {
-    console.log(error);
-  }
-};
+//     return await sgMail.send(msg);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
 exports.sendEmail = async (args) => {
   if (process.env.NODE_ENV === 'development') {
