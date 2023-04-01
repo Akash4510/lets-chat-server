70d1bf81f7f4a400634041c5d74649eae11d7664
Akash <ag308669@gmail.com>
2023-04-01 17:41:42 +0530

    Renamed verify-otp route to verify-email

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ const authController = require('../controllers/authController');
 router.post('/login', authController.login);
 router.post('/register', authController.register, authController.sendOtp);
 router.post('/send-otp', authController.sendOtp);
-router.post('/verify-otp', authController.verifyOtp);
+router.post('/verify-email', authController.verifyEmail);
 router.post('/forgot-password', authController.forgotPassword);
 router.post('/reset-password', authController.resetPassword);
 
